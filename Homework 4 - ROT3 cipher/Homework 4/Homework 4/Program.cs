0d090162db17c2cf38ef784af7bdae4bd7bf9a15
nchdelia <nechita.delia9@gmail.com>
2023-12-29 10:10:16 +0200

    Homework 4 - ROT3 cipher

---
+++ Homework 4 - ROT3 cipher/Homework 4/Homework 4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Homework_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //citirea stringului de la tastatura
+            Console.Write("Hello! Write a word / text: ");
+            string text = Console.ReadLine();
+
+            //conversia stringului
+            string alphabet = "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ"; //string creat pentru verificare
+            StringBuilder sb = new StringBuilder(); //declararea noului string
+            
+            for (int i = 0; i < text.Length; i++)
+            {   
+                if (alphabet.IndexOf(text[i]) != -1)
+                {
+                    int letterValue = (int)text[i] + 3;
+                    char letter = (char)letterValue;
+                    sb.Append(letter);
+                } else
+                {
+                    sb.Append(text[i]);
+                }
+            }
+
+            //afisarea noului string
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}

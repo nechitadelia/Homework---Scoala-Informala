2c184f758e5077dfbc3e5e86d56be0e5f28cd9fd
nchdelia <nechita.delia9@gmail.com>
2024-01-05 10:00:15 +0200

    Homework 7 - Search for a number

---
+++ Homework 7 - Search for a number/Homework 7/Homework 7/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Homework_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //citirea unui numar de la tastatura
+            Console.Write("Hello! Write a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            //declararea array-ului de numere
+            int[] numberArray = new int[10] { 34, 643, -521, 76, 12, 0, 782, 97, 51, 74 };
+
+            //cautarea numarului in array
+            int result = SearchNumber(number, numberArray);
+
+            //afisarea rezultatului
+            if (result == -1)
+            {
+                Console.WriteLine("Sorry, the number was not found in the array.");
+            } else
+            {
+                Console.WriteLine($"The number was found in the array at index: {result}");
+            }
+
+        }
+
+        static int SearchNumber(int num, int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == num)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}

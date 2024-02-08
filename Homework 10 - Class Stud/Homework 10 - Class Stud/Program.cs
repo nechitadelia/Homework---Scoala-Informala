84fbc757aa78c766cf873c60173fbb6ed22a969b
nchdelia <nechita.delia9@gmail.com>
2024-02-08 09:37:31 +0200

    Homework 10 - Class Student Assignment

---
+++ Homework 10 - Class Stud/Homework 10 - Class Stud/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Homework_10___Class_Stud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //crearea celor 5 obiecte de tip Student
+            Student[] students = new Student[5];
+
+            for (byte i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine($"________STUDENT NUMBER {i+1}________");
+                Console.Write("Write a name: ");
+                string userName = Console.ReadLine();
+
+                do
+                {
+                    Console.Write("Write an age: ");
+                    bool success = byte.TryParse(Console.ReadLine(), out byte userAge);
+                    students[i] = new Student(userAge, userName);
+                } 
+                while (students[i].Age == 0);
+            }
+
+            //afisarea informatiilor studentilor la consola
+            Console.WriteLine("________STUDENTS INFO________");
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student.Info);
+            }
+
+            //citirea de la tastatura a notelor
+            Console.WriteLine("________STUDENT MARKS________");
+            Console.WriteLine($"Give marks for students. If not, press Enter.");
+
+            for (byte i = 0; i < students.Length; i++)
+            {
+                Console.Write($"Student number {i+1}: ");
+                string mark = Console.ReadLine();
+                if (mark != "")
+                {
+                    students[i].Mark = Convert.ToByte(mark);
+                }
+                else
+                {
+                    students[i].Mark = null;
+                }
+            }
+
+            //calcularea si afisarea la consola a mediei studentilor cu nota
+            double sum = 0;
+            byte counter = 0;
+
+            for (byte i = 0; i < students.Length; i++)
+            {
+                if (students[i].Mark != null)
+                {
+                    sum += (double)students[i].Mark;
+                    counter++;
+                }
+            }
+
+            Console.WriteLine($"The average mark for all students is: {sum / counter}");
+        }
+    }
+}
---
+++ Homework 10 - Class Stud/Homework 10 - Class Stud/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_10___Class_Stud
+{
+    public class Student
+    {
+        //campuri
+        private byte _age;
+        private string _name;
+        private const byte MIN_AGE = 18;
+        private const byte MAX_AGE = 99;
+
+
+        //proprietati
+        public byte Age
+        {
+            get { return _age; }
+            set
+            {   
+                if (value >= MIN_AGE && value <= MAX_AGE)
+                {
+                    _age = value;
+                } 
+                else
+                {
+                    Console.WriteLine("Incorrect input! Write an age between 18-99!");
+                }
+            }
+        }
+        public string Name { get; set; }
+
+        public byte? Mark { get; set; }
+        public dynamic Info
+        {
+            get { return $"Name: {Name};\n Age: {Age}."; }
+        }
+
+        //constructor
+        public Student(byte age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+    }
+}

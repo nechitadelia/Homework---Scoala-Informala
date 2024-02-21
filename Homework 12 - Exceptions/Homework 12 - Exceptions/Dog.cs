f9c14ea222ab8505f8df25109b799305ed64a231
nchdelia <nechita.delia9@gmail.com>
2024-02-21 10:39:40 +0200

    Homework 12 - Dog with Exceptions Assignment

---
+++ Homework 12 - Exceptions/Homework 12 - Exceptions/Dog.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_12___Exceptions
+{
+    public class Dog
+    {
+        //campuri
+        private int _age;
+        private string _name;
+
+        //proprietati
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                try
+                {
+                    AgeChecking(value);
+                    _age = value;
+                    Console.WriteLine("The age is valid.");
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e);
+                }
+                catch (ApplicationException e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                try
+                {
+                    NameChecking(value);
+                    _name = value;
+                    Console.WriteLine("The name is valid.");
+                }
+                catch(ApplicationException e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        //metode
+        static void AgeChecking(int age)
+        {
+            if (age < 0)
+            {
+                throw new ArgumentException("Age cannot be negative. Choose a positive number for age!");
+            }
+            if (age > 100)
+            {
+                throw new ApplicationException("Write an age less than or equal to 100!");
+            }
+        }
+
+        static void NameChecking(string name)
+        {
+            if (name.Length < 2)
+            {
+                throw new ApplicationException($"{name} is not a valid name!");
+            }
+        }
+    }
+}
---
+++ Homework 12 - Exceptions/Homework 12 - Exceptions/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework_12___Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //verificarea cazului de input corect
+            //Dog dog = new Dog { Age = 10, Name = "Bobby"};
+
+            //verificarea cazului in care varsta e negativa
+            //Dog dog = new Dog { Age = -10, Name = "Bobby" };
+
+            //verificarea cazului in care varsta e mai mare de 100
+            //Dog dog = new Dog { Age = 150, Name = "Bobby" };
+
+            //verificarea cazului in care numele are mai putin de 2 litere
+            Dog dog = new Dog { Age = 10, Name = "B" };
+
+        }
+    }
+}
